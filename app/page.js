2561d87bb07ac0138b1c7b102b92b44a8a1dcd6f
Mufty-arifin <arifinmufty@gmail.com>
2025-01-03 22:32:53 +0700

    update navbar, hero

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Navbar />
+      <Hero/>
     </div>
   );
 }
